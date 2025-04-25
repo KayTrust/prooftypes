b6a476238d3b7416d795e49e431cf135d86cd11a
Geiner Grandez Valle <ggrandez@emeal.nttdata.com>
2025-04-25 07:10:02 -0500

    chore: export extra values

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import {JwtCredentialPayload} from 'did-jwt-vc'
 export {ProofTypeJWT, ResolverOrOptions} from './proof-type-jwt'
 export {ProofType} from './proof-type'
 export { CredentialPayload, JwtCredentialPayload, VerifiedCredential, PresentationPayload, JwtPresentationPayload, VerifiedPresentation } from 'did-jwt-vc'
+
+export type CredentialStatus = JwtCredentialPayload["vc"]["credentialStatus"]

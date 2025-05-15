753cb0b98144ebbed4d85d2ee7191c18e49707fb
Geiner Grandez Valle <ggrandez@emeal.nttdata.com>
2025-05-15 10:42:04 -0500

    chore: added types issuer

--- src/proof-type-jwt.ts
+++ src/proof-type-jwt.ts
@@ -1,12 +1,15 @@
-import { Issuer, CredentialPayload, JwtCredentialPayload, VerifiedCredential, JwtPresentationPayload, VerifiedPresentation, createVerifiableCredentialJwt, createVerifiablePresentationJwt, verifyCredential, verifyPresentation } from 'did-jwt-vc'
-import { EthrDID } from '@kaytrust/did-ethr'
+import { CredentialPayload, JwtCredentialPayload, VerifiedCredential, JwtPresentationPayload, VerifiedPresentation, createVerifiableCredentialJwt, createVerifiablePresentationJwt, verifyCredential, verifyPresentation } from 'did-jwt-vc'
+import { Issuer as IssuerBase } from '@kaytrust/did-base'
 import { ProofType } from "./proof-type";
 import { Resolver } from 'did-resolver'
 import { getResolver } from 'ethr-did-resolver'
 
-import type {CreatePresentationOptions, PresentationPayload, VerifyPresentationOptions} from 'did-jwt-vc'
+import type {CreatePresentationOptions, Issuer as IssuerDid, PresentationPayload, VerifyPresentationOptions} from 'did-jwt-vc'
+import { EthrDID } from '@kaytrust/did-ethr';
 
 type VerifyOptions = VerifyPresentationOptions
+
+type Issuer = IssuerBase | IssuerDid | EthrDID
 
 interface InfuraConfiguration {
     infuraProjectId: string
@@ -25,7 +28,7 @@ export type VerifyOptionsProofTypeJWT = {
 } & Omit<VerifyOptions, 'resolver'>
 
 type ProofTypeJWTConfig = {
-    issuer?: Issuer|EthrDID,
+    issuer?: Issuer,
     resolver?: ResolverOrOptions,
     verifyOptions?: VerifyOptions,
 }
@@ -34,8 +37,8 @@ export class ProofTypeJWT<
     IP extends boolean = false, 
     D extends (IP extends true ? (JwtPresentationPayload | PresentationPayload) : (JwtCredentialPayload | CredentialPayload)) = (IP extends true ? (JwtPresentationPayload | PresentationPayload) : (JwtCredentialPayload | CredentialPayload)),
     VR extends (IP extends true ? VerifiedPresentation : VerifiedCredential) = (IP extends true ? VerifiedPresentation : VerifiedCredential),
-> extends ProofType<D, string, Issuer|EthrDID, VR> {
-    private issuer?: Issuer|EthrDID;
+> extends ProofType<D, string, Issuer, VR> {
+    private issuer?: Issuer;
     private resolver?: ResolverOrOptions;
     private verifyOptions?: VerifyOptions;
     readonly is_presentation: IP
@@ -62,11 +65,12 @@ export class ProofTypeJWT<
         if (this.is_presentation) return verifyPresentation(verifiableObjectWithProof, resolver, verifyOptions) as Promise<VR>
         return verifyCredential(verifiableObjectWithProof, resolver, verifyOptions) as Promise<VR>
     }
-    generateProof(verifiableObject: D, issuer?: Issuer|EthrDID, options?: CreatePresentationOptions): Promise<string> {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    generateProof(verifiableObject: D, issuer?: any, options?: CreatePresentationOptions): Promise<string> {
         if (!issuer) issuer = this.issuer;
         if (!issuer) throw new Error("missing_issuer: No issuer has been configured");
-        if (this.is_presentation) return createVerifiablePresentationJwt(verifiableObject as any, issuer as Issuer, {...options, header: {kid: verifiableObject.iss||verifiableObject.issuer||this.issuer?.did, ...options?.header}})
-        return createVerifiableCredentialJwt(verifiableObject as any, issuer as Issuer, {...options, header: {kid: verifiableObject.iss||verifiableObject.issuer, ...options?.header}})
+        if (this.is_presentation) return createVerifiablePresentationJwt(verifiableObject as any, issuer as IssuerBase, {...options, header: {kid: verifiableObject.iss||verifiableObject.issuer||this.issuer?.did, ...options?.header}})
+        return createVerifiableCredentialJwt(verifiableObject as any, issuer as IssuerBase, {...options, header: {kid: verifiableObject.iss||verifiableObject.issuer, ...options?.header}})
     }
 
     readonly proofType = 'JwtProof2020';
